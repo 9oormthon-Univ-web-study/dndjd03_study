6f6015eb32ae38e18367395f2f97afa76d2775cf
Chaewon Kim <rud15dns@naver.com>
2024-08-25 15:58:50 +0900

    성능개선방법

---
+++ react-performance-app/src/App.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import A from './component/A';
+import B from './component/B';
+
+function App() {
+
+  const [value, setvalue] = useState("");
+  const [posts, setposts] = useState([]);
+  
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+    .then(posts => setposts(posts))  
+  }, [])
+  
+  return (
+    <div style={{padding : '1rem'}}>
+      <input 
+        value={value}
+        onChange={e => setvalue(e.target.value)} 
+      />
+
+      <div style={{ display : 'flex' }}>
+        <A message={value} posts={posts}/>
+        <B message={value} posts={posts}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
